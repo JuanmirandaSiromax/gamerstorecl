cab4f6c5fec082d65b6eda1d7045a14844f82e15
Francisca Valdivia Palma <franciscavaldivia@Franciscas-MacBook-Pro.local>
2023-12-16 15:17:57 -0300

    Se añade funcionalidad para agregar y borrar juegos del ccarrito

--- mientretencion/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from.models import Categoria,Juego,UserProfile
-
-
-# Register your models here.
-admin.site.register(Categoria)
-admin.site.register(Juego)
-admin.site.register(UserProfile)
