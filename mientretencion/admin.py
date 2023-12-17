57a645cbc9210559651e82cd438266b107fd9d27
Francisca Valdivia Palma <franciscavaldivia@Franciscas-MacBook-Pro.local>
2023-12-17 18:22:34 -0300

    Se suben cambios

--- mientretencion/admin.py
+++ mientretencion/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from.models import Categoria,Juego,UserProfile
+from.models import Categoria,Juego,UserProfile,MensajeContacto
 
 
 # Register your models here.
 admin.site.register(Categoria)
 admin.site.register(Juego)
 admin.site.register(UserProfile)
+admin.site.register(MensajeContacto)
